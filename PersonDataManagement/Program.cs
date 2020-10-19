305209b4bc159a2288343f3b6d59447e6d876bd0
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-10-19 17:15:02 +0530

    [Srushti] Add.Get Top Two persons from persons list having age less than 60

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 namespace PersonDataManagement
 {
     class Program
@@ -8,7 +9,7 @@ namespace PersonDataManagement
         {
             List<Person> personList = new List<Person>();
             AddRecords(personList);
-
+            RetrievingTopTwoRecordsForAgeLessThanSixty(personList);
         }
         private static void AddRecords(List<Person> listPersonInCity)
         {
@@ -22,6 +23,13 @@ namespace PersonDataManagement
             listPersonInCity.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65));
             listPersonInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore,NY", 85));
             listPersonInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore,NY", 95));
+        }
+        public static void RetrievingTopTwoRecordsForAgeLessThanSixty(List<Person> personList)
+        {
+            foreach (Person p in personList.FindAll(e => (e.Age < 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name :" + p.Name + " Age :" + p.Age);
+            }
         }
     }
 }
